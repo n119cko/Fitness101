3d33c36147e021068bffce593dd4f415dc64bbe3
Nick van Tilburg <119nvt@gmail.com>
2024-09-29 16:02:16 +1000

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Nick van Tilburg on 29/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
